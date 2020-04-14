a8a4a9bcc7857df7a0a773df8a1813277156be94
Dave Williams <cdave@uw.edu>
2020-04-14 15:32:56 -0700

    Run the notebook via nbcovert rather than exec
    Exec has depth/inheritance limits that prevent runing functions in
    functions with imports within a notebook.

--- workflow_colony_formation/utils/jupyterstep.py
+++ workflow_colony_formation/utils/jupyterstep.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pickle
+from pathlib import Path
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
 from datastep import Step
 
 
@@ -10,18 +12,20 @@ class JupyterStep(Step):
 
     def __init__(self, **kwargs):
         # Run Step init
-        super().__init__(kwargs)
+        super().__init__(**kwargs)
 
     def run(self, nb_name=None, **kwargs):
         """Run the notebook"""
+        # TODO Needs to write config out to pkl for access by notebook
         self.args = kwargs
-        config_pkl = pickle.dumps(self)
-        nbfn = Path(__file__).parent / (self.name + ".ipynb")
-        nb = nbformat.read(str(nbfn), 4)
-        for cell in nb["cells"]:
-            if cell["cell_type"] == "code":
-                exec(cell["source"])
-        self = pickle.loads(config)
+        config = self.__dict__
+        nbpath = Path(__file__).parent.parent / 'steps' / self.step_name
+        nbfn = self.name + ".ipynb"
+        with open(nbpath / nbfn) as f:
+            nb = nbformat.read(f, as_version=4)
+        executer = ExecutePreprocessor(timeout=60*60, kernel_name='python3')
+        executer.preprocess(nb, {'metadata': {'path': str(nbpath)}})
+        self.__dict__.update(config)
         self.manifest.to_csv(
             self.step_local_staging_dir / Path("manifest.csv"), index=False
         )
