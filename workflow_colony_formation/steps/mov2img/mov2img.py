9acbf574b151d617b4be69c1593cb7f4f8308984
Dave Williams <cdave@uw.edu>
2020-04-20 15:12:00 -0700

    Wrap run in log_run_params

--- workflow_colony_formation/steps/mov2img/mov2img.py
+++ workflow_colony_formation/steps/mov2img/mov2img.py
@@ -18,3 +18,7 @@ log = logging.getLogger(__name__)
 class Mov2Img(JupyterStep):
     def __init__(self, direct_upstream_tasks = ["null2mov"], **kwargs):
         super().__init__(direct_upstream_tasks=direct_upstream_tasks, **kwargs)
+
+    @log_run_params
+    def run(self, **kwargs):
+        super().run(**kwargs)
