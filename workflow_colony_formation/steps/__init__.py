aff9092c8ff87cf8f3a2bfe0c9c0ded4c9c61199
Dave Williams <cdave@uw.edu>
2020-04-29 11:47:21 -0700

    Add Seg2Cen, calculates centroids and areas
    Has Img2Seg as upstream task.

--- workflow_colony_formation/steps/__init__.py
+++ workflow_colony_formation/steps/__init__.py
@@ -3,5 +3,6 @@
 from .null2mov import Null2Mov
 from .mov2img import Mov2Img
 from .img2seg import Img2Seg
+from .seg2cen import Seg2Cen
 
-__all__ = ["Null2Mov", "Mov2Img", "Img2Seg"]
+__all__ = ["Null2Mov", "Mov2Img", "Img2Seg", "Seg2Cen"]
---
+++ workflow_colony_formation/steps/seg2cen/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from .seg2cen import Seg2Cen  # noqa: F401
+
+__all__ = ["Seg2Cen"]
