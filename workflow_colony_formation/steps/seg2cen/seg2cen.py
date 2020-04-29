10fdeb557e3c2a9b9e96491157b7526e1176bcac
Dave Williams <cdave@uw.edu>
2020-04-29 11:59:07 -0700

    Black formatting

--- workflow_colony_formation/steps/seg2cen/seg2cen.py
+++ workflow_colony_formation/steps/seg2cen/seg2cen.py
@@ -15,7 +15,7 @@ log = logging.getLogger(__name__)
 
 
 class Seg2Cen(JupyterStep):
-    def __init__(self, direct_upstream_tasks = ["img2seg"], **kwargs):
+    def __init__(self, direct_upstream_tasks=["img2seg"], **kwargs):
         super().__init__(direct_upstream_tasks=direct_upstream_tasks, **kwargs)
 
     @log_run_params
